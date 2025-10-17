41013e6ff3af2dd24236aa26f3ba104f57b595af
rahul.prasad <rahul.prasad@geekywolf.com>
2025-10-17 15:21:09 +0530

    completed c# assignments

---
+++ Day 4/c#/Question1.cs
@@ -0,0 +1,57 @@
+﻿//using System.Diagnostics;
+
+//namespace MyProgram
+//{
+//    public class MyProgram
+//    { 
+//        static void Main(string[] args)
+//        {
+//            EmployeeManagement employee1 = new EmployeeManagement("John Doe", 15000, "Permanent");
+//            EmployeeManagement employee2 = new EmployeeManagement("Liam Smith", 20000, "Contract");
+//            EmployeeManagement employee3 = new EmployeeManagement("Mary James", 15000, "Permanent");
+            
+//            Console.WriteLine(employee1.EmployeeDetails());
+//            Console.WriteLine(employee2.EmployeeDetails());
+//            Console.WriteLine(employee3.EmployeeDetails());
+            
+//        }
+//    }
+//    public class EmployeeManagement
+//    {
+//        public string Id { get; private set; }
+//        private static int IdCounter;
+//        public string Name { get; set; }
+//        public double Salary { get; set; }
+//        public string EmployeeType { get; set; }
+
+//        static EmployeeManagement()
+//        {
+//            IdCounter = 1000;
+//        }
+
+//        public EmployeeManagement(string name, double salary, string employeeType)
+//        {
+//            IdCounter++;
+//            Id = $"Emp{IdCounter}";
+//            Name = name;
+//            Salary = salary;
+//            EmployeeType = employeeType;
+//        }
+
+//        public static int GetNumOfEmployee()
+//        {
+//            return IdCounter - 1000;
+//        }
+
+//        public static string GetNextAvailableId()
+//        {
+//            return $"Emp{IdCounter + 1}";
+//        }
+
+//        public string EmployeeDetails()
+//        {
+//            return $"Employee Id {Id},Employee Name {Name}, Employee Type {EmployeeType}, Salary {Salary}";
+//        }
+//    }
+//}
+
---
+++ Day 4/c#/Question2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Ride ride1 = new Ride("Amit Verma", "Rohan Singh", 10);
+            Ride ride2 = new Ride("Priya Nair", "Anjali Gupta", 5);
+            Ride ride3 = new Ride("Karan Malhotra", "Neha Sharma", 12);
+
+            ride1.ShowRideDetails();
+            ride2.ShowRideDetails();
+            ride3.ShowRideDetails();
+
+            Ride.ShowRideSummary();
+        }
+    }
+    public class Ride
+    {
+        
+        public static int totalRides {  get; set; }
+        public static double totalEarnings { get; set; }    
+        public static double baseFare { get; set; }
+        public static double surgeMultiplier { get; set; } 
+        public static int counter = 1000;
+
+        public string RideId { get; set; }
+        public string DriverName { get; set; }
+        public string PassengerName { get; set; }
+        public double DistanceKm { get; set; }
+        public double Fare { get; set; }
+
+
+        static Ride() {
+            totalRides = 0;
+            totalEarnings = 0;
+            surgeMultiplier = 1.0;
+            baseFare = 50;
+            Console.WriteLine("Uber System Initialized. Ready to book rides...");
+        }
+
+        public Ride(string driverName, string passengerName, double distanceKm)
+        {
+            totalRides++;
+            counter++;
+            RideId = $"Ride_{counter}";
+            DriverName = driverName ;   
+            PassengerName = passengerName ;
+            DistanceKm = distanceKm ;
+
+            Fare = FareCalculation(distanceKm);
+            totalEarnings = totalEarnings + Fare;
+        }
+
+        public double FareCalculation(double distanceKm)
+        {
+            return baseFare + (distanceKm * 15 * surgeMultiplier);
+        }
+
+        public static void SergeMultiplier(double multiplier)
+        {
+            surgeMultiplier = multiplier;
+        }
+
+        public static void ShowRideSummary()
+        {
+            Console.WriteLine("Total Rides : "+ totalRides);
+            Console.WriteLine("Total Earnings : "+ totalEarnings);
+        }
+
+        public void ShowRideDetails()
+        {
+            Console.WriteLine($"Ride Id {RideId}, Driver {DriverName}, Passanger {PassengerName}, Distance {DistanceKm}, Fare {Fare}");
+        }
+    }
+}
