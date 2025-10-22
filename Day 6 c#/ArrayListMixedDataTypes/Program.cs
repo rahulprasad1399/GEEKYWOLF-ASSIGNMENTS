880aaf0faea4a156ae42569909c090d9f5d74d61
rahul.prasad <rahul.prasad@geekywolf.com>
2025-10-22 18:59:22 +0530

    day6 c# assignments

---
+++ Day 6 c#/ArrayListMixedDataTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace ArrayListMixedDataTypes
+{
+    public class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            MixDataTypes mixdata = new MixDataTypes();
+            mixdata.AddStringItem("Harry");
+            mixdata.AddIntegerItem(25);
+            mixdata.AddDoubleItem(75.5);
+            mixdata.AddBoolItem(true);
+            mixdata.DisplayItem();
+        }
+    }
+
+    public class MixDataTypes
+    {
+        public ArrayList arrayList = new ArrayList();
+        public void AddStringItem(string item)
+        {
+            arrayList.Add(item);
+        }
+        public void AddIntegerItem(int item)
+        {
+            arrayList.Add(item);
+        }
+        public void AddDoubleItem(double item)
+        {
+            arrayList.Add(item);
+        }
+        public void AddBoolItem(bool item)
+        {
+            arrayList.Add(item);
+        }
+        public void DisplayItem()
+        {
+            foreach (var item in arrayList) { 
+            Console.WriteLine(item+" "+item.GetType());
+            }
+        }
+    }
+}
---
+++ Day 6 c#/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagmentSystem;
+
+namespace consoleApp
+{
+    public class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            EmployeeManagment employee = new EmployeeManagment();
+            employee.AddEmployee("EMP1001", "Harry", 2000, "Permanent");
+            employee.DisplayEmployee();
+        }
+    }
+}
