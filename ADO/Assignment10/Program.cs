f52aa07cf924777f92c65f792b74e2e205477382
rahul.prasad <rahul.prasad@geekywolf.com>
2025-10-28 15:58:36 +0530

    completed ado assignments

---
+++ ADO/Assignment10/Program.cs
@@ -0,0 +1,57 @@
+﻿// 10)  Create a parameterized procedure to update a student using stored procedure “UpdateStudent”
+// with input parameters (@StudentId, @StudnetName, @Class) and print student updated successfully
+// or not.
+
+//CREATE PROCEDURE UpdateStudent
+//    @StudentId INT,
+//    @StudentName VARCHAR(100),
+//    @Class VARCHAR(50)
+//AS
+//BEGIN
+//    UPDATE Student
+//    SET Name = @StudentName,
+//        Class = @Class
+//    WHERE Id = @StudentId;
+
+//IF @@ROWCOUNT > 0
+//        PRINT 'Student updated successfully.';
+//ELSE
+//    PRINT 'No student found with the given ID.';
+//END;
+
+using Microsoft.Data.SqlClient;
+
+namespace Assignment10
+{
+    public class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            string cs = "Data Source=localhost;Initial Catalog=ado_db;Integrated Security=False;User ID=sa;Password=Rahul@123;TrustServerCertificate=True;";
+            UpdateStudent(cs, "Rohit", "MCA", 2);
+        }
+
+        public static void UpdateStudent(string cs, string studentName, string className, int studentId)
+        {
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UpdateStudent", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@StudentId", studentId);
+                cmd.Parameters.AddWithValue("@StudentName", studentName);
+                cmd.Parameters.AddWithValue("@Class", className);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Student Updated Successfully");
+                } else
+                {
+                    Console.WriteLine("No Student found with the provided Id");
+                }
+            }
+        }
+    }
+}
---
+++ ADO/Question9/Program.cs
@@ -0,0 +1,50 @@
+﻿// 9) Create a parameterized procedure to delete a student using stored procedure “DeleteStudent”
+// with input parameters (@StudentId) and print student deleted successfully or not.
+
+//CREATE PROCEDURE DeleteStudent
+//    @StudentId INT
+//AS
+//BEGIN
+//    DELETE FROM Student WHERE Id = @StudentId;
+
+//IF @@ROWCOUNT > 0
+//        PRINT 'Student deleted successfully.';
+//ELSE
+//    PRINT 'No student found with the given ID.';
+//END;
+
+using Microsoft.Data.SqlClient;
+
+namespace Question9
+{
+    public class Assignment9
+    {
+        public static void Main(string[] args)
+        {
+            string cs = "Data Source=localhost;Initial Catalog=ado_db;Integrated Security=False;User ID=sa;Password=Rahul@123;TrustServerCertificate=True;";
+            DeleteStudent(cs, 2);
+        }
+
+        public static void DeleteStudent(string cs, int studentId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("DeleteStudent", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@StudentId", studentId);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                
+                if(rowsAffected > 0)
+                {
+                    Console.WriteLine("Student deleted successfully");
+                } else
+                {
+                    Console.WriteLine("No Student found with the given id");
+                }
+            }
+        }
+    }
+}
